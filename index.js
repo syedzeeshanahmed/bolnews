761be540be03e093d8ae124430248296bf90b390
syedzeeshan1987 <s.zeeshan47@gmail.com>
2023-01-29 20:47:51 +0500

    Using template string

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const name = "chandan";
+// const name = "chandan";
+// alert(name);
 
 
-alert(name);
+
+let fname = "Syed";
+let mname = "Zee";
+let lname = "ahmed";
+
+let age = prompt("Zeeshan's age is");
+
+// old way
+// let result = fname + ' ' + mname + ' ' + lname + ' ' + 'is' + ' ' + age;
+// alert(result);
+
+
+// using template string
+
+let result = `${fname} ${mname} ${lname} is ${age} years old`;
+alert(result);
